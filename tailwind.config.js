4c42930e851f42041bee8619e038e299cb621b8b
krishna <tksumanth1994@gmail.com>
2022-06-28 14:42:18 +0530

    feat: add lint-staged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
-      white: "#ffffff",
-      black: "#111111",
-      primary: "#E7B400",
-      secondary: "#E78B00"
+      white: '#ffffff',
+      black: '#111111',
+      primary: '#E7B400',
+      secondary: '#E78B00',
     },
     fontFamily: {
-      'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
-      'mono': ['Red Hat Mono', ...defaultTheme.fontFamily.mono]
+      sans: ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      mono: ['Red Hat Mono', ...defaultTheme.fontFamily.mono],
     },
     extend: {
       boxShadow: {
-        'default': '6px 6px 0px'
-      }
+        default: '6px 6px 0px',
+      },
     },
   },
   plugins: [],
-}
+};

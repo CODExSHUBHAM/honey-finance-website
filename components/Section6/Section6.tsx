a0a2f7a69e96ba009c0b01e04d9012d511b6dd30
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-04 22:27:48 +0530

    feat: Added Section 6

---
+++ components/Section6/Section6.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Icon from '../ui/Icon/Icon';
+import Button from '../ui/Button/Button';
+import Heading from '../ui/Heading/Heading';
+import Box from '../ui/Box/Box';
+import Wormhole from '@/assets/images/Wormhole.png';
+import Katana from '@/assets/images/Katana.png';
+import Solvent from '@/assets/images/Solvent.png';
+
+function Section6(): JSX.Element {
+  return (
+    <div className="bg-white bg-opacity-95 py-10 px-2">
+      <Box className="py-8 px-2 bg-gray-50 flex flex-col text-center items-center">
+        <Heading variant="black" className="font-bold">
+          Partenered with
+        </Heading>
+        <Button
+          as="a"
+          className="flex items-center gap-2 w-fit mt-6 "
+          variant="gray"
+          size="sm"
+          href=""
+          target="_blank"
+        >
+          Mirror.xyz
+          <Icon icon="LinkArrow" />
+        </Button>
+        <div className="flex flex-col w-fit pt-8 gap-8">
+          <Image src={Wormhole} alt="WormholeLogo" width={150} height={150} />
+          <Image src={Katana} alt="KatanaLogo" width={150} height={150} />
+          <Image src={Solvent} alt="SolventLogo" />
+        </div>
+      </Box>
+    </div>
+  );
+}
+
+export default Section6;

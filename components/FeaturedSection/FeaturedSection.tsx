9cdd92c0b9faa897e87f54a8b6666d2d4a91a77c
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-06 11:24:22 +0530

    fix: fixed featured section

--- components/FeaturedSection/FeaturedSection.tsx
+++ components/FeaturedSection/FeaturedSection.tsx
@@ -1,52 +1,27 @@
-import Image from 'next/image';
-import Featured1 from '@/assets/images/featured/Featured1.png';
+import Featured1 from '@/assets/images/featured/Featured1.svg';
 import Featured2 from '@/assets/images/featured/Featured2.png';
 import Featured3 from '@/assets/images/featured/Featured3.png';
 import Featured4 from '@/assets/images/featured/Featured4.png';
-import Featured5 from '@/assets/images/featured/Featured5.png';
+import Featured5 from '@/assets/images/featured/Featured5.svg';
 
 function FeaturedSection(): JSX.Element {
+  console.log(Featured1);
   return (
-    <div className="bg-neutral-50 flex flex-col items-center justify-center gap-10 py-4 md:py-10">
-      <p className="text-black uppercase text-lg font-normal leading-5">
+    <div className="bg-neutral-50 flex flex-col items-center justify-center gap-10 py-20 md:py-10">
+      <p className="font-mono text-black uppercase text-lg font-normal leading-5">
         As Featured In
       </p>
-      <div className="flex flex-col md:flex-row items-center justify-center gap-8">
-        <Image
-          src={Featured1}
-          alt="Featured1"
-          width={250}
-          height={40}
-          layout="fixed"
+
+      <div className="flex flex-col md:flex-row items-center justify-center md:justify-around w-full gap-12">
+        <img src={Featured1.src} alt="Company logo" className="h-10 w-auto " />
+        <img src={Featured2.src} alt="NFT nyc Logo" className="h-12 w-auto " />
+        <img
+          src={Featured3.src}
+          alt="Web3 wire Logo"
+          className="h-20 w-auto "
         />
-        <Image
-          src={Featured2}
-          alt="Featured2"
-          width={250}
-          height={60}
-          layout="fixed"
-        />
-        <Image
-          src={Featured3}
-          alt="Featured3"
-          width={250}
-          height={100}
-          layout="fixed"
-        />
-        <Image
-          src={Featured4}
-          alt="Featured4"
-          width={250}
-          height={50}
-          layout="fixed"
-        />
-        <Image
-          src={Featured5}
-          alt="Featured5"
-          width={250}
-          height={40}
-          layout="fixed"
-        />
+        <img src={Featured4.src} alt="Superteam logo" className="h-8 w-auto " />
+        <img src={Featured5.src} alt="Company logo" className="h-10 w-auto " />
       </div>
     </div>
   );

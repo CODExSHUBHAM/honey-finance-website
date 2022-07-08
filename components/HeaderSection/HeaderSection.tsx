4f3b51ca9c81c7cf979f8eb5d2bf4f1c0150c37a
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-08 13:33:13 +0530

    fix: fixed header image

--- components/HeaderSection/HeaderSection.tsx
+++ components/HeaderSection/HeaderSection.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Heading from '@/components/ui/Heading/Heading';
 import Button from '../ui/Button/Button';
 import HeaderImage from '@/assets/images/header-image.png';
@@ -8,11 +7,11 @@ function HeaderSection(): JSX.Element {
     <div className="header-section md:min-h-screen bg-cover bg-right-top bg-no-repeat flex flex-col justify-center items-center mt-10 px-4 pb-16 ">
       <div className="flex flex-col gap-2  md:flex-row-reverse md:items-center md:gap-4 max-w-7xl lg:max-w-full mx-auto md:px-0">
         <div>
-          <Image
-            src={HeaderImage}
+          <img
+            src={HeaderImage.src}
             alt="Header-Image"
             className="w-full h-auto z-0"
-          ></Image>
+          />
         </div>
         <div className="flex flex-col gap-4 md:gap-6 mt-[-30px]">
           <Heading variant="black" size="sm" className="font-semibold">

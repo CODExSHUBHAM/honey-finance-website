73f49b21e6ffe192f7657211afe4be6cc3a032d6
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-08 13:28:10 +0530

    fix: fixed spacing issues in header section

--- components/HeaderSection/HeaderSection.tsx
+++ components/HeaderSection/HeaderSection.tsx
@@ -5,8 +5,8 @@ import HeaderImage from '@/assets/images/header-image.png';
 
 function HeaderSection(): JSX.Element {
   return (
-    <div className="header-section md:min-h-screen bg-cover bg-right-top bg-no-repeat flex flex-col justify-center items-center mt-10 px-4 py-4">
-      <div className="flex flex-col  md:flex-row-reverse md:items-center md:gap-4 max-w-7xl lg:max-w-full mx-auto md:px-0">
+    <div className="header-section md:min-h-screen bg-cover bg-right-top bg-no-repeat flex flex-col justify-center items-center mt-10 px-4 pb-16 ">
+      <div className="flex flex-col gap-2  md:flex-row-reverse md:items-center md:gap-4 max-w-7xl lg:max-w-full mx-auto md:px-0">
         <div>
           <Image
             src={HeaderImage}
@@ -14,7 +14,7 @@ function HeaderSection(): JSX.Element {
             className="w-full h-auto z-0"
           ></Image>
         </div>
-        <div className="flex flex-col gap-2 md:gap-6 mt-[-30px]">
+        <div className="flex flex-col gap-4 md:gap-6 mt-[-30px]">
           <Heading variant="black" size="sm" className="font-semibold">
             Making NFT Lending and Staking easy.
           </Heading>

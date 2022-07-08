8b3ff3ca2e570033f3197c5bb0dec7f22b5cf445
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-08 13:04:07 +0530

    feat: added title and Favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,36 @@ import Section4 from '@/components/Section4/Section4';
 import Section5 from '@/components/Section5/Section5';
 import Section6 from '@/components/Section6/Section6';
 import FooterSection from '@/components/FooterSection/FooterSection';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <div className="relative min-h-full flex flex-col">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+        <title>Honey Finance</title>
+      </Head>
       <Navbar />
       <HeaderSection />
       <FeaturedSection />

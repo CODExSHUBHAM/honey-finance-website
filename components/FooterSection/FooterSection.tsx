aa791a3f318ebe7780c35a026db77d79dacbf0a6
CODExSHUBHAM <Shubham.nh10@gmail.com>
2022-07-15 20:35:44 +0530

    feat: optimised footer section for desktop

--- components/FooterSection/FooterSection.tsx
+++ components/FooterSection/FooterSection.tsx
@@ -8,69 +8,81 @@ import Box from '../ui/Box/Box';
 
 function FooterSection(): JSX.Element {
   return (
-    <div className="bg-white px-1 py-16 md:p-20 flex flex-col md:flex-row items-center text-center md:text-left">
-      <div>
-        <Heading size="sm" variant="black" className="font-bold">
-          Honey Finance is run by our DAO and operated by Honey Labs.
-        </Heading>
-        <div className="flex flex-col md:flex-row md:gap-10 items-center">
-          <Button
-            as="a"
-            className="flex items-center gap-2 justify-center w-10/12 md:w-fit mt-6 "
-            variant="gray"
+    <div className="bg-white px-1 py-16 md:p-20">
+      <div className="flex flex-col md:flex-row items-center text-center md:text-left w-fit mx-auto md:gap-40">
+        <div>
+          <Heading
             size="sm"
-            href=""
-            target="_blank"
+            variant="black"
+            className="font-bold md:text-3xl md:w-[34rem]"
           >
-            Documentation
-            <Icon icon="LinkArrow" />
-          </Button>
-          <Button
-            as="a"
-            className="flex items-center gap-2 w-10/12 justify-center md:w-fit mt-6 "
-            variant="gray"
-            size="sm"
-            href=""
-            target="_blank"
-          >
-            Whitepaper
-            <Icon icon="LinkArrow" />
-          </Button>
+            Honey Finance is run by our DAO and operated by Honey Labs.
+          </Heading>
+          <div className="flex flex-col md:flex-row md:gap-10 items-center">
+            <Button
+              as="a"
+              className="flex items-center gap-2 justify-center w-10/12 md:w-fit mt-6 "
+              variant="gray"
+              size="sm"
+              href=""
+              target="_blank"
+            >
+              Documentation
+              <Icon icon="LinkArrow" />
+            </Button>
+            <Button
+              as="a"
+              className="flex items-center gap-2 w-10/12 justify-center md:w-fit mt-6 "
+              variant="gray"
+              size="sm"
+              href=""
+              target="_blank"
+            >
+              Whitepaper
+              <Icon icon="LinkArrow" />
+            </Button>
+          </div>
+          <div className="hidden md:flex items-center gap-8 w-fit mt-20">
+            <Icon icon="Discord"></Icon>
+            <Icon icon="Telegram" />
+            <Icon icon="Twitter" />
+          </div>
         </div>
-        <div className="hidden md:flex items-center gap-8 w-fit mt-20">
+        <div className="md:flex items-center justify-center md:w-[50rem]">
+          <Box
+            shadowType="DARK"
+            className="py-6 px-2 flex flex-col items-center text-center md:w-[40rem]"
+          >
+            <Image src={Logo} alt="Honey Logo" />
+            <Heading size="sm" variant="black" className="font-bold pt-3">
+              Honey DAO
+            </Heading>
+            <p className="font-mono text-sm font-normal leading-5 text-gray-400 pt-4">
+              Governed by the Honey token, the DAO allows stakeholders to decide
+              vote on strategy, treasury, emissions, and insurance funds of the
+              protocol.
+            </p>
+          </Box>
+          <Box
+            shadowType="DARK"
+            className="py-6 px-2 flex flex-col items-center text-center md:w-[40rem]"
+          >
+            <Image src={Potion} alt="Honey Logo" />
+            <Heading size="sm" variant="black" className="font-bold pt-3">
+              Honey Labs
+            </Heading>
+            <p className="font-mono text-sm font-normal leading-5 text-gray-400 pt-4">
+              Honey Labs builds the protocol and proposes changes to the DAO. It
+              oversees day to day operations and implements changes to the
+              protocol.
+            </p>
+          </Box>
+        </div>
+        <div className="md:hidden flex items-center gap-14 w-full justify-center mt-8">
           <Icon icon="Discord" />
           <Icon icon="Telegram" />
           <Icon icon="Twitter" />
         </div>
-      </div>
-      <div className="md:flex">
-        <Box shadowType="DARK" className="py-6 px-2 flex flex-col items-center">
-          <Image src={Logo} alt="Honey Logo" />
-          <Heading size="sm" variant="black" className="font-bold pt-3">
-            Honey DAO
-          </Heading>
-          <p className="font-mono text-sm font-medium leading-5 text-gray-400 pt-4">
-            Governed by the Honey token, the DAO allows stakeholders to decide
-            vote on strategy, treasury, emissions, and insurance funds of the
-            protocol.
-          </p>
-        </Box>
-        <Box shadowType="DARK" className="py-6 px-2 flex flex-col items-center">
-          <Image src={Potion} alt="Honey Logo" />
-          <Heading size="sm" variant="black" className="font-bold pt-3">
-            Honey Labs
-          </Heading>
-          <p className="font-mono text-sm font-medium leading-5 text-gray-400 pt-4">
-            Honey Labs builds the protocol and proposes changes to the DAO. It
-            oversees day to day operations and implements changes to the
-            protocol.
-          </p>
-        </Box>
-      </div>
-      <div className="md:hidden flex items-center gap-14 w-full justify-center mt-8">
-        <Icon icon="Discord" />
-        <Icon icon="Telegram" />
-        <Icon icon="Twitter" />
       </div>
     </div>
   );
--- components/Section6/Section6.tsx
+++ components/Section6/Section6.tsx
@@ -9,14 +9,14 @@ import Solvent from '@/assets/images/Solvent.png';
 function Section6(): JSX.Element {
   return (
     <div className="bg-white bg-opacity-[0.95] py-20 md:px-20">
-      <Box className="py-16 px-2 bg-gray-50 flex flex-col md:flex-row text-center items-center md:justify-between md:px-20">
-        <div className="flex flex-col justify-center items-center">
+      <Box className="py-16 px-2 bg-gray-50 flex flex-col md:flex-row text-center items-center md:justify-between md:px-20 md:w-fit md:gap-56 md:mx-auto">
+        <div className="flex flex-col justify-center items-center md:items-start">
           <Heading variant="black" className="font-bold">
             Partenered with
           </Heading>
           <Button
             as="a"
-            className="flex items-center gap-2 justify-center mt-14 w-10/12 "
+            className="flex items-center gap-2 justify-center mt-14 w-10/12 md:w-fit"
             variant="gray"
             size="sm"
             href=""
@@ -26,9 +26,9 @@ function Section6(): JSX.Element {
             <Icon icon="LinkArrow" />
           </Button>
         </div>
-        <div className="flex flex-col md:flex-row items-center pt-16  gap-12">
-          <img src={Wormhole.src} alt="WormholeLogo" className="w-48" />
-          <img src={Katana.src} alt="KatanaLogo" className="w-48" />
+        <div className="flex flex-col md:flex-row items-center pt-16 md:pt-0  gap-20">
+          <img src={Wormhole.src} alt="WormholeLogo" className="w-48 md:w-40" />
+          <img src={Katana.src} alt="KatanaLogo" className="w-48 md:w-40  " />
           <img src={Solvent.src} alt="SolventLogo" className="w-48" />
         </div>
       </Box>
